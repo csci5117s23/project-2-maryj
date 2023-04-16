2e6db35306dd80ed3ff8a415d764888cdaf02f3f
minizou <gh.mini.zou@gmail.com>
2023-04-16 18:51:51 -0500

    Add styling to match Figma

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { MdHomeFilled, MdFavorite, MdStar, MdLogout } from "react-icons/md";
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css'
 
@@ -7,24 +8,20 @@ home likes starred logout jenkins logo
 
 export default function Header() {
   const links = [
-    { href: '/home', text: 'home',  icon: 'idk'},
-    { href: '/likes', text: 'likes',  icon: 'idk'},
-    { href: '/starred', text: 'starred',  icon: 'idk'},
-    { href: '/logout', text: 'logout',  icon: 'idk'},
+    { href: '/home', text: 'Home',  icon: <MdHomeFilled />},
+    { href: '/likes', text: 'Likes',  icon: <MdFavorite />},
+    { href: '/starred', text: 'Starred',  icon: <MdStar />},
+    { href: '/logout', text: 'Logout',  icon: <MdLogout />},
   ]
 
   return (
     <div className={styles.header}>
       <div className={styles.links}>
         {links.map((link, index) => (
-          <div key={index} className={styles.link}>
-            <Link href={link.href}>
-              {link.text}{' '}
-            </Link>
-            <div className={styles.icon}>
-              
-            </div>
-          </div>
+          <Link key={index} href={link.href} className={styles.link}>
+            {link.text}{' '}
+            <div className={styles.icon}>{link.icon}</div>
+          </Link>
         ))}
       </div>
       <div className={styles.logo}>
