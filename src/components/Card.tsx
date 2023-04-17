3e6cf02733d5df53c7f75fb823421fe9885c3a7b
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2023-04-16 21:16:11 -0500

    star filling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import { MdStar, MdStarBorder } from "react-icons/md";
 import styles from '@/styles/Card.module.css';
+import { useState } from 'react';
 
 interface CardProps {
     id: number;
@@ -12,6 +13,8 @@ interface CardProps {
 }
 
 export default function Card({ id, title, category, image, isStarred }: CardProps) {
+    const [starState, setStarState]: [boolean, Function] = useState(isStarred);
+
     return (
         <div className={styles.card}>
             <div className={styles['image-container']}>
@@ -21,9 +24,19 @@ export default function Card({ id, title, category, image, isStarred }: CardProp
                     alt={`image of ${title}`} 
                     fill 
                 />
-                {isStarred ? 
-                    <MdStar color="yellow" size="25px" className={styles['image-star']} /> : 
-                    <MdStarBorder color="yellow" size="25px" className={styles['image-star']} />
+                {starState ? 
+                    <MdStar 
+                        className={styles['image-star']}
+                        color="yellow" 
+                        size="25px"
+                        onClick={e => setStarState(!starState)}
+                    /> : 
+                    <MdStarBorder 
+                        className={styles['image-star']} 
+                        color="yellow" 
+                        size="25px" 
+                        onClick={e => setStarState(!starState)} 
+                    />
                 }
             </div>
             <h1 className={styles.title}>{title}</h1>
