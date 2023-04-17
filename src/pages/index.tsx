41a57814b80e47a40bf25eae37b6e929db3c9442
Youssef Zahar <zahar022@umn.edu>
2023-04-16 21:33:32 -0500

    fixed index ☺️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
 import { Inter } from 'next/font/google'
-import CardContainer from '@/components/CardContainer'
-import Header from '@/components/Header';
-import { useAuth } from '@clerk/nextjs';
-// import SignInPage from './signin';
 import Splash from './splash';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      Splash
-    </>
+    <Splash />
   );
 }
